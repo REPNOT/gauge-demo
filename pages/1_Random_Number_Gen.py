f4794a508a213db48a16707090a1bf89c30edd00
Derek <38998364+REPNOT@users.noreply.github.com>
2023-10-22 06:14:02 -0400

    Update

--- pages/1_Random_Number_Gen.py
+++ pages/1_Random_Number_Gen.py
@@ -14,7 +14,7 @@ def rand_control(cVal):
     file_str = f"{str(Path.cwd())}/config.json"
 
     with open(file_str, "r") as fileObj:
-        tempData =  dict(fileObj.read())
+        tempData =  fileObj.load()
 
     tempData["rand_button"] = control_val
 
@@ -31,7 +31,7 @@ def rand_state():
     file_str = f"{str(Path.cwd())}/config.json"
 
     with open(file_str, "r") as fileObj:
-        tempData =  dict(fileObj.read())
+        tempData =  fileObj.load()
 
     return tempData["rand_button"]
 
