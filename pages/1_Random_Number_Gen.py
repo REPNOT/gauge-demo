7df8e10dab576c1038cad1b59cf93977992828aa
Derek <38998364+REPNOT@users.noreply.github.com>
2023-10-22 17:42:47 +0000

    Update

--- pages/1_Random_Number_Gen.py
+++ pages/1_Random_Number_Gen.py
@@ -7,27 +7,6 @@ import json
 from pathlib import Path
 
 
-def rand_gen():
-
-    for count in range(1, 20):
-        time.sleep(1)
-        st.rerun()
-
-    return
-
-
-with st.sidebar:
-
-    st.button("STOP", type="primary")
-
-    if st.button("RUN"):
-        for count in range(1, 20):
-            time.sleep(1)
-            st.rerun()
-    else:
-        st.stop()
-
-
 col1, col2 = st.columns(2)
 
 with col1:
@@ -36,4 +15,14 @@ with col1:
 with col2:
     gauge(random(), gSize="MED")
 
+with st.sidebar:
 
+    st.button("STOP", type="primary")
+
+    if st.button("RUN"):
+        for count in range(1, 20):
+            time.sleep(1)
+
+            st.rerun()
+    else:
+        st.stop()
