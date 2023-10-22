116902201eb91fb6654d58707beebc976590c01c
Derek <38998364+REPNOT@users.noreply.github.com>
2023-10-22 05:27:37 +0000

    Update

--- pages/new_page.py
+++ pages/new_page.py
@@ -1,5 +1,15 @@
 import plotly.graph_objects as go
 import streamlit as st
 from easyGauge import easy_gauge
+import random
+import time
 
-easy_gauge(.55)
+
+gaugeDrive = 0.0
+
+for count in range(1, 1000):
+
+    gaugeDrive = random()
+
+
+easy_gauge(gaugeDrive)
