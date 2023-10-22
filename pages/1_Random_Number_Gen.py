e901f193c8c8cfb0975fff4604b9286c1251641c
Derek <38998364+REPNOT@users.noreply.github.com>
2023-10-22 06:31:09 -0400

    Update

--- pages/1_Random_Number_Gen.py
+++ pages/1_Random_Number_Gen.py
@@ -6,36 +6,6 @@ import time
 import json
 from pathlib import Path
 
-
-def rand_control(cVal):
-
-    control_val = cVal
-
-    file_str = f"{str(Path.cwd())}/config.json"
-
-    with open(file_str, "r") as fileObj:
-        tempData =  json.load(fileObj)
-
-    tempData["rand_button"] = control_val
-
-    with open(file_str, "w") as fileObj:
-        json.dump(tempData, fileObj, indent=4)
-
-
-    return st.rerun()
-
-
-def rand_state():
-
-    file_str = f"{str(Path.cwd())}/config.json"
-
-    with open(file_str, "r") as fileObj:
-        tempData =  json.load(fileObj)
-
-    st.write(tempData)
-
-    return tempData["rand_button"]
-
 col1, col2 = st.columns(3)
 
 with col1:
@@ -47,18 +17,6 @@ with col2:
 st.button("On", type="primary", on_click=rand_control("true"), use_container_width=True)
 st.button("Off", type="secondary", on_click=rand_control("false"), use_container_width=True)
 
-rand_gen = rand_state()
-
-
-if rand_gen == "false":
-    run_rand = False
-else:
-    run_rand = True
-
-if rand_gen:
-
-    for count in range(1, 50):
-        time.sleep(.5)
-        st.rerun()
-
-
+for count in range(1, 50):
+    time.sleep(.5)
+    st.rerun()
