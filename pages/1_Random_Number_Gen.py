793d1e91f340744ac4cb2b63e7604f6d97468392
Derek <38998364+REPNOT@users.noreply.github.com>
2023-10-22 06:09:13 -0400

    CONTROL BUTTONS

--- pages/1_Random_Number_Gen.py
+++ pages/1_Random_Number_Gen.py
@@ -3,34 +3,57 @@ import streamlit as st
 from streamViz import gauge
 from random import random
 import time
+import json
+from pathlib import Path
 
 
-def addSB():
+def rand_control(cVal):
 
-    add_selectbox = st.sidebar.selectbox(
-        "How would you like to be contacted?",
-        ("Email", "Home phone", "Mobile phone")
-    )
-    return add_selectbox
+    control_val = cVal
 
-st.sidebar.button("Reset", type="primary", callback=addSB)
+    file_str = f"{str(Path.cwd())}\\config.json"
+
+    with open(file_str, "r") as fileObj:
+        tempData =  fileObj.read()
+
+    tempData["rand_button"] = control_val
+
+    with open(file_str, "w") as fileObj:
+        json.dump(tempData, fileObj, indent=4)
+
+    st.rerun()
+
+    return "<===  Random Number Button Cliked  ===>"
 
 
-with st.container():
+def rand_state():
 
-    col1, col2, col3 = st.columns(3)
+    file_str = f"{str(Path.cwd())}\\config.json"
 
-    with col1:
-        gauge(random(), gSize="LRG")
+    with open(file_str, "r") as fileObj:
+        tempData =  fileObj.read()
 
-    with col2:
-        gauge(random(), gSize="LRG")
+    return tempData["rand_button"]
 
-    with col3:
-        gauge(random(), gSize="LRG")
+st.sidebar.button("On", type="primary", on_click=rand_control("true"))
+st.sidebar.button("Off", type="primary", on_click=rand_control("false"))
 
-    for count in range(1, 50):
-        time.sleep(.5)
-        st.rerun()
+rand_gen = rand_state()
+
+col1, col2 = st.columns(3)
+
+with col1:
+    gauge(random(), gSize="LRG")
+
+with col2:
+    gauge(random(), gSize="LRG")
+
+
+
+
+
+for count in range(1, 50):
+    time.sleep(.5)
+    st.rerun()
 
 
