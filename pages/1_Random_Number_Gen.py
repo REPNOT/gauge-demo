8b6ed26ebda450d4bbc83fe3239b070051ca92eb
Derek <38998364+REPNOT@users.noreply.github.com>
2023-10-22 18:16:56 +0000

    Update

--- pages/1_Random_Number_Gen.py
+++ pages/1_Random_Number_Gen.py
@@ -10,20 +10,19 @@ from pathlib import Path
 col1, col2 = st.columns(2)
 
 with col1:
-    gauge(random(), gSize="SML", gMode="number")
+    gauge(random(), gSize="SML", sFix="%")
 
 with col2:
-    gauge(random(), gSize="SML")
+    gauge(random(), gSize="SML", gMode="number")
 
 
 col3, col4 = st.columns(2)
 
 with col3:
-    gauge(random(), gSize="LRG", sFix="%")
+    gauge(random(), gSize="MED", sFix="%")
 
 with col4:
-    gauge(random(), gSize="LRG", gMode="number", sFix="%")
-    gauge(random(), gSize="SML", gMode="number")
+    gauge(random(), gSize="MED", gMode="number")
 
 
 with st.sidebar:
