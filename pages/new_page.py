a233df31d6a37527a97f429df89ee70ca0431045
Derek <38998364+REPNOT@users.noreply.github.com>
2023-10-22 05:36:19 +0000

    update

--- pages/new_page.py
+++ pages/new_page.py
@@ -4,10 +4,4 @@ from easyGauge import easy_gauge
 from random import random
 import time
 
-
-gaugeDrive = 0.0
-
-for count in range(1, 1000):
-
-    time.sleep(1)
-    easy_gauge(random())
+easy_gauge(random())
