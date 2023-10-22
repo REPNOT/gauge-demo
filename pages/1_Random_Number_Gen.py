56baa2f27b06b018deac7d47344cd29c830fcd61
Derek <38998364+REPNOT@users.noreply.github.com>
2023-10-22 04:19:12 -0400

    Update

--- pages/1_Random_Number_Gen.py
+++ pages/1_Random_Number_Gen.py
@@ -1,12 +1,11 @@
 import plotly.graph_objects as go
 import streamlit as st
-from easyGauge import easy_gauge
+from streamViz import gauge
 from random import random
 import time
-from streamlit.hello.utils import show_code
 
 
-easy_gauge(random())
+gauge(random())
 
 for count in range(1, 50):
     time.sleep(.5)
