3c318be332624f07200d2a1ae011cfd18be76977
Derek <38998364+REPNOT@users.noreply.github.com>
2023-10-22 04:50:29 +0000

    Updates

--- pages/new_page.py
+++ pages/new_page.py
@@ -6,37 +6,37 @@ def easyGauge(gVal, gTitle="", gMode='gauge+number', gSize="MED",
                     gcHigh='#1B8720', xpLeft=0, xpRight=1, ypBot=0, ypTop=1, 
                     arBot=None, arTop=1, pTheme="streamlit", cWidth=True, sFix=None):
 
-    """Deploy Plotly streamGauge or indicator data visualization
+    """Deploy Plotly gauge or indicator data visualization
 
     Keyword arguments:
 
-    gVal -- streamGauge Value (required)
+    gVal -- gauge Value (required)
         Description:
             The value passed to this argument is displayed in
             the center of the visualization, drives the color & position
-            of the streamGauge and is required to successfully call this function.
+            of the gauge and is required to successfully call this function.
         Data Type:
             integer, float
 
-    gTitle -- streamGauge Title (default '')
+    gTitle -- gauge Title (default '')
         Description:
             Adds a header displayed at the top
             of the visualization.
         Data Type:
             string
 
-    gMode -- streamGauge Mode (default streamGauge+number)
+    gMode -- gauge Mode (default gauge+number)
         Description:
             Declares which type of visualization should
             be displayed.
         Options:
-            streamGauge+number, streamGauge, number
+            gauge+number, gauge, number
         Data Type:
             string
 
-    gSize -- streamGauge Size (default MED)
+    gSize -- gauge Size (default MED)
         Description:
-            Automatically resizes the streamGauge or indicator using 
+            Automatically resizes the gauge or indicator using 
             pre-defined values options.
 
             The size of visualization can also be customized by passing the 'CUST' value to
@@ -47,20 +47,20 @@ def easyGauge(gVal, gTitle="", gMode='gauge+number', gSize="MED",
         Data Type:
             String
 
-    grLow -- Low streamGauge Range (default 0.30)
+    grLow -- Low gauge Range (default 0.30)
         Description:
-            Sets the bottom (lowest) percentile target group for the streamGauge value.  
-            When the streamGauge Value (gVal) is less than the value assigned to this
-            argument, the color assigned to the gcLow (Low streamGauge Color) argument
+            Sets the bottom (lowest) percentile target group for the gauge value.  
+            When the gauge Value (gVal) is less than the value assigned to this
+            argument, the color assigned to the gcLow (Low gauge Color) argument
             is displayed.
         Data Type:
             integer, float
 
-    grMid -- Middle streamGauge Range (default 0.70) -- 
+    grMid -- Middle gauge Range (default 0.70) -- 
         Description:
-            Sets the middle percentile target group for the streamGauge value.  When
-            the streamGauge Value (gVal) is less than the value assigned to this argument,
-            the color assigned to the gcMid (Middle streamGauge Color) argument is displayed.
+            Sets the middle percentile target group for the gauge value.  When
+            the gauge Value (gVal) is less than the value assigned to this argument,
+            the color assigned to the gcMid (Middle gauge Color) argument is displayed.
             
             If the value assigned to the gVal argument is greater than or equal to
             the value assigned to the grMid argument, the color value assigned to
@@ -68,38 +68,38 @@ def easyGauge(gVal, gTitle="", gMode='gauge+number', gSize="MED",
         Data Type:
             integer, float
 
-    gcLow -- Low streamGauge Color (default #FF1708)
+    gcLow -- Low gauge Color (default #FF1708)
         Description:
-            streamGauge color for bottom percentile target group. Default value
+            gauge color for bottom percentile target group. Default value
             is a hex code for red.  Argument excepts hex color codes and 
             there associated names.
         Data Type:
             string
 
-    gcMid -- Middle streamGauge Color (default #FF9400)
+    gcMid -- Middle gauge Color (default #FF9400)
         Description:
-            streamGauge color for middle percentile target group. Default value
+            gauge color for middle percentile target group. Default value
             is a hex code for orange.  Argument excepts hex color codes and 
             there associated names.
         Data Type:
             string
 
-    gcHigh -- High streamGauge Color (default #1B8720)
+    gcHigh -- High gauge Color (default #1B8720)
         Description:
-            streamGauge color for middle percentile target group. Default value
+            gauge color for middle percentile target group. Default value
             is a hex code for green.  Argument excepts hex color codes and 
             there associated names.
         Data Type:
             string
 
-    sFix -- streamGauge Value Suffix (default 0.0)
+    sFix -- gauge Value Suffix (default 0.0)
         Description:
-            Adds a suffix (character) to the streamGauge value displayed in the
+            Adds a suffix (character) to the gauge value displayed in the
             center of the visualization.
             
             Assigning the '%' character to this argument will display the
             percentage symbol at the end of the value shown in the center
-            of the visualization and convert the streamGauge value from a floating
+            of the visualization and convert the gauge value from a floating
             point integer so the value displays correctly as a percentage.
         Options:
             %
@@ -118,19 +118,19 @@ def easyGauge(gVal, gTitle="", gMode='gauge+number', gSize="MED",
 
     if sFix == "%":
 
-        streamGaugeVal = round((gVal * 100), 1)
+        gaugeVal = round((gVal * 100), 1)
         top_axis_range = (arTop * 100)
         bottom_axis_range = arBot
-        low_streamGauge_range = (grLow * 100)
-        mid_streamGauge_range = (grMid * 100)
+        low_gauge_range = (grLow * 100)
+        mid_gauge_range = (grMid * 100)
 
     else:
 
-        streamGaugeVal = gVal
+        gaugeVal = gVal
         top_axis_range = arTop
         bottom_axis_range = arBot
-        low_streamGauge_range = grLow
-        mid_streamGauge_range = grMid
+        low_gauge_range = grLow
+        mid_gauge_range = grMid
 
     if gSize == "SML":
         x1, x2, y1, y2 =.25, .25, .75, 1
@@ -143,22 +143,22 @@ def easyGauge(gVal, gTitle="", gMode='gauge+number', gSize="MED",
     elif gSize == "CUST":
         x1, x2, y1, y2 = xpLeft, xpRight, ypBot, ypTop   
 
-    if streamGaugeVal < low_streamGauge_range: 
-        streamGaugeColor = gcLow
-    elif streamGaugeVal >= low_streamGauge_range and streamGaugeVal < mid_streamGauge_range:
-        streamGaugeColor = gcMid
+    if gaugeVal < low_gauge_range: 
+        gaugeColor = gcLow
+    elif gaugeVal >= low_gauge_range and gaugeVal < mid_gauge_range:
+        gaugeColor = gcMid
     else:
-        streamGaugeColor = gcHigh
+        gaugeColor = gcHigh
 
     fig1 = go.Figure(go.Indicator(
         mode = gMode,
-        value = streamGaugeVal,
+        value = gaugeVal,
         domain = {'x': [x1, x2], 'y': [y1, y2]},
         number = {"suffix": sFix},
         title = {'text': gTitle},
-        streamGauge = {
+        gauge = {
             'axis': {'range': [bottom_axis_range, top_axis_range]},
-            'bar' : {'color': streamGaugeColor}
+            'bar' : {'color': gaugeColor}
         }
     ))
 
