d0bdb5bca157a9da23490d2c4ce55a3ce74f3f45
Derek <38998364+REPNOT@users.noreply.github.com>
2023-10-23 09:59:53 +0000

    Update

--- pages/1_Data_Demo.py
+++ pages/1_Data_Demo.py
@@ -1,13 +1,24 @@
-from streamOps import rd_json_file
 import json
 from pathlib import Path
 from pprint import pprint
 import streamlit as st
+from streamViz import gauge
+from streamOps import rd_json_file
 
+@st.cache_data
+def rd_data_file():
 
+    file_path = f"{str(Path.cwd())}/data/electricity_supply_disposition.json"
 
-# years_lst, states_lst, states_lst = rd_json_file('data', 'electricity_supply_disposition')
-states_lst = rd_json_file('data', 'electricity_supply_disposition')['metadata']['states']
-# columns_lst = rd_json_file('data', 'electricity_supply_disposition')['metadata']['columns']
+    with open(file_path, "r") as fileObj:
+        rawData = json.load(fileObj)[0]
 
-print(pprint(states_lst))
+    return rawData
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.selectbox("Year", options=rd_data_file()['metadata']['years'])
+
+with col2:
+    st.selectbox("State", options=rd_data_file()['metadata']['states'])
