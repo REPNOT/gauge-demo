573033ef1ce08391ea3908b0d74bea88d8db389a
Derek <38998364+REPNOT@users.noreply.github.com>
2023-10-23 08:43:42 +0000

    EIA DATA UPDATES - DATA SUMMARIES - GOOD

--- pages/1_Data_Demo.py
+++ pages/1_Data_Demo.py
@@ -6,10 +6,8 @@ from pprint import pprint
 file_str = f"{str(Path.cwd())}/data/electricity_supply_disposition.json"
 
 with open(file_str, "r") as fileObj:
-    energyData = json.load(fileObj)
+    energyData = json.load(fileObj)[0]
 
 years_lst = energyData['metadata']['years']
 states_lst = energyData['metadata']['states']
 columns_lst = energyData['metadata']['columns']
-
-energyData = energyData['data']
