d122af7e6924778e05762ea343403dda9a6414c5
Derek <38998364+REPNOT@users.noreply.github.com>
2023-10-23 06:12:24 +0000

    Minor Changes - Good

---
+++ pages/1_Data_Demo.py
@@ -0,0 +1,15 @@
+import json
+from pathlib import Path
+from pprint import pprint
+
+
+file_str = f"{str(Path.cwd())}/data/electricity_supply_disposition.json"
+
+with open(file_str, "r") as fileObj:
+    energyData = json.load(fileObj)
+
+years_lst = energyData['metadata']['years']
+states_lst = energyData['metadata']['states']
+columns_lst = energyData['metadata']['columns']
+
+energyData = energyData['data']
