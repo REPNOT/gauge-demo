e17c301ef08fc46938244c139417827708422cf0
Derek <38998364+REPNOT@users.noreply.github.com>
2023-10-26 19:19:02 -0400

    Update

--- pages/4_Documentation.py
+++ pages/4_Documentation.py
@@ -112,7 +112,7 @@ with st.expander("Source Code - streamViz.py"):
     
     st.divider()
 
-    funcCode1 = """
+    funcCode = """
         import plotly.graph_objects as go
         import streamlit as st
 
@@ -122,9 +122,6 @@ with st.expander("Source Code - streamViz.py"):
                 gcHigh='#1B8720', xpLeft=0, xpRight=1, ypBot=0, ypTop=1, 
                 arBot=None, arTop=1, pTheme="streamlit", cWidth=True, sFix=None):
 
-    """
-
-    funcCode2 = """
         \t\"\"\"
                 Deploy Plotly gauge or indicator data visualization
 
@@ -235,75 +232,73 @@ with st.expander("Source Code - streamViz.py"):
                     pTheme -- Plot Theme (default 0.0)
                     cWidth -- Container Width (default 0.0)
             \t\"\"\"
+
+            if sFix == "%":
+
+                gaugeVal = round((gVal * 100), 1)
+                top_axis_range = (arTop * 100)
+                bottom_axis_range = arBot
+                low_gauge_range = (grLow * 100)
+                mid_gauge_range = (grMid * 100)
+
+            else:
+
+                gaugeVal = gVal
+                top_axis_range = arTop
+                bottom_axis_range = arBot
+                low_gauge_range = grLow
+                mid_gauge_range = grMid
+
+            if gSize == "SML":
+                x1, x2, y1, y2 =.25, .25, .75, 1
+            elif gSize == "MED":
+                x1, x2, y1, y2 = .50, .50, .50, 1
+            elif gSize == "LRG":
+                x1, x2, y1, y2 = .75, .75, .25, 1
+            elif gSize == "FULL":
+                x1, x2, y1, y2 = 0, 1, 0, 1
+            elif gSize == "CUST":
+                x1, x2, y1, y2 = xpLeft, xpRight, ypBot, ypTop   
+
+            if gaugeVal <= low_gauge_range: 
+                gaugeColor = gcLow
+            elif gaugeVal >= low_gauge_range and gaugeVal <= mid_gauge_range:
+                gaugeColor = gcMid
+            else:
+                gaugeColor = gcHigh
+
+            fig1 = go.Figure(go.Indicator(
+                mode = gMode,
+                value = gaugeVal,
+                domain = {'x': [x1, x2], 'y': [y1, y2]},
+                number = {"suffix": sFix},
+                title = {'text': gTitle},
+                gauge = {
+                    'axis': {'range': [bottom_axis_range, top_axis_range]},
+                    'bar' : {'color': gaugeColor}
+                }
+            ))
+
+            config = {'displayModeBar': False}
+            fig1.update_traces(title_font_color=gTheme, selector=dict(type='indicator'))
+            fig1.update_traces(number_font_color=gTheme, selector=dict(type='indicator'))
+            fig1.update_traces(gauge_axis_tickfont_color=gTheme, selector=dict(type='indicator'))
+            fig1.update_layout(margin_b=5)
+            fig1.update_layout(margin_l=20)
+            fig1.update_layout(margin_r=20)
+            fig1.update_layout(margin_t=50)
+
+            fig1.update_layout(margin_autoexpand=True)
+
+            st.plotly_chart(
+                fig1, 
+                use_container_width=cWidth, 
+                theme=pTheme, 
+                **{'config':config}
+            )
     """
 
-    funcCode3 = """
-        if sFix == "%":
 
-            gaugeVal = round((gVal * 100), 1)
-            top_axis_range = (arTop * 100)
-            bottom_axis_range = arBot
-            low_gauge_range = (grLow * 100)
-            mid_gauge_range = (grMid * 100)
-
-        else:
-
-            gaugeVal = gVal
-            top_axis_range = arTop
-            bottom_axis_range = arBot
-            low_gauge_range = grLow
-            mid_gauge_range = grMid
-
-        if gSize == "SML":
-            x1, x2, y1, y2 =.25, .25, .75, 1
-        elif gSize == "MED":
-            x1, x2, y1, y2 = .50, .50, .50, 1
-        elif gSize == "LRG":
-            x1, x2, y1, y2 = .75, .75, .25, 1
-        elif gSize == "FULL":
-            x1, x2, y1, y2 = 0, 1, 0, 1
-        elif gSize == "CUST":
-            x1, x2, y1, y2 = xpLeft, xpRight, ypBot, ypTop   
-
-        if gaugeVal <= low_gauge_range: 
-            gaugeColor = gcLow
-        elif gaugeVal >= low_gauge_range and gaugeVal <= mid_gauge_range:
-            gaugeColor = gcMid
-        else:
-            gaugeColor = gcHigh
-
-        fig1 = go.Figure(go.Indicator(
-            mode = gMode,
-            value = gaugeVal,
-            domain = {'x': [x1, x2], 'y': [y1, y2]},
-            number = {"suffix": sFix},
-            title = {'text': gTitle},
-            gauge = {
-                'axis': {'range': [bottom_axis_range, top_axis_range]},
-                'bar' : {'color': gaugeColor}
-            }
-        ))
-
-        config = {'displayModeBar': False}
-        fig1.update_traces(title_font_color=gTheme, selector=dict(type='indicator'))
-        fig1.update_traces(number_font_color=gTheme, selector=dict(type='indicator'))
-        fig1.update_traces(gauge_axis_tickfont_color=gTheme, selector=dict(type='indicator'))
-        fig1.update_layout(margin_b=5)
-        fig1.update_layout(margin_l=20)
-        fig1.update_layout(margin_r=20)
-        fig1.update_layout(margin_t=50)
-
-        fig1.update_layout(margin_autoexpand=True)
-
-        st.plotly_chart(
-            fig1, 
-            use_container_width=cWidth, 
-            theme=pTheme, 
-            **{'config':config}
-        )
-    """
-
-    funcCode = f"{funcCode1}{funcCode2}{funcCode3}"
 
     st.code(funcCode, language='python')
 
