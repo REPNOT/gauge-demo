0e906f04002f92ee57ddc1000347d5ad835096b0
Derek Evans <derek.evans@deinfoservice.com>
2023-11-01 14:44:10 -0400

    Update

--- Home.py
+++ Home.py
@@ -13,95 +13,95 @@ def run():
         initial_sidebar_state="collapsed"
     )
 
-  with st.container():
+    with st.container():
 
-      def local_css(file_name):
-          with open(file_name) as f:
-              st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
+        def local_css(file_name):
+            with open(file_name) as f:
+                st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
 
-      styleCSS = local_css("style/style.css")
+        styleCSS = local_css("style/style.css")
 
-      st.markdown(f"""
-          <div class="tabs-menu">
-              <ul class="tabs-menu">
-                  <li class="tabs-menu">
-                      <a class="tabs-menu"href="https://rev-gauge.streamlit.app" target="_self">
-                          Home
-                      </a>
-                  </li class="tabs-menu">
-                  <li class="tabs-menu">
-                      <a class="tabs-menu" href="https://rev-gauge.streamlit.app/Introduction_Demo" target="_self">
-                          Introduction Demo
-                      </a>
-                  </li>
-                  <li class="tabs-menu">
-                      <a class="tabs-menu" href="https://rev-gauge.streamlit.app/Dashboard_Demo" target="_self">
-                          Dashboard Demo
-                      </a>
-                  </li> 
-                  <li class="tabs-menu">
-                      <a class="tabs-menu" href="https://rev-gauge.streamlit.app/Interactive_Demo" target="_self">
-                          Interactive Demo
-                      </a>
-                  </li>
-                  <li class="tabs-menu">
-                      <a class="tabs-menu" href="https://rev-gauge.streamlit.app/Automated_Demo" target="_self">
-                          Automated Demo
-                      </a>
-                  </li> 
-                  <li class="tabs-menu">
-                      <a class="tabs-menu" href="https://rev-gauge.streamlit.app/Documentation" target="_self">
-                          Documentation
-                      </a>
-                  </li>
-                  <li class="tabs-menu">
-                      <a class="tabs-menu" href="https://rev-gauge.streamlit.app/Video_Demo" target="_self">
-                          Video Demo
-                      </a>
-                  </li>
-              </ul>
-          </div>
-      """, unsafe_allow_html=True
-      )
+        st.markdown(f"""
+            <div class="tabs-menu">
+                <ul class="tabs-menu">
+                    <li class="tabs-menu">
+                        <a class="tabs-menu"href="https://rev-gauge.streamlit.app" target="_self">
+                            Home
+                        </a>
+                    </li class="tabs-menu">
+                    <li class="tabs-menu">
+                        <a class="tabs-menu" href="https://rev-gauge.streamlit.app/Introduction_Demo" target="_self">
+                            Introduction Demo
+                        </a>
+                    </li>
+                    <li class="tabs-menu">
+                        <a class="tabs-menu" href="https://rev-gauge.streamlit.app/Dashboard_Demo" target="_self">
+                            Dashboard Demo
+                        </a>
+                    </li> 
+                    <li class="tabs-menu">
+                        <a class="tabs-menu" href="https://rev-gauge.streamlit.app/Interactive_Demo" target="_self">
+                            Interactive Demo
+                        </a>
+                    </li>
+                    <li class="tabs-menu">
+                        <a class="tabs-menu" href="https://rev-gauge.streamlit.app/Automated_Demo" target="_self">
+                            Automated Demo
+                        </a>
+                    </li> 
+                    <li class="tabs-menu">
+                        <a class="tabs-menu" href="https://rev-gauge.streamlit.app/Documentation" target="_self">
+                            Documentation
+                        </a>
+                    </li>
+                    <li class="tabs-menu">
+                        <a class="tabs-menu" href="https://rev-gauge.streamlit.app/Video_Demo" target="_self">
+                            Video Demo
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        """, unsafe_allow_html=True
+        )
 
-  st.write("# Plotly Indicator Gauge Demo App")
+    st.write("# Plotly Indicator Gauge Demo App")
 
-  st.markdown(
-    """
+    st.markdown(
+        """
 
-      <p>
-      A <a href="https://streamlit.io/" target="_blank">Streamlit</a> demonstration application for utilizing a custom <a href="https://www.python.org/" target="_blank">Python</a> module
-      that helps developers quickly incorporate <a href="https://plotly.com/python/" target="_blank">Plotly</a> Gauge visualizations
-      into their projects with the help of preset parameters. The application includes multiple
-      demonstrations and interactive examples to help users become familiar with the module.  Additional information about the application, including
-      source code, dependencies, tools used for development, and parameters, can be found in the <a href="https://rev-gauge.streamlit.app/Documentation" target="_blank">documentation</a>
-      section the site.
-      </p>
+        <p>
+        A <a href="https://streamlit.io/" target="_blank">Streamlit</a> demonstration application for utilizing a custom <a href="https://www.python.org/" target="_blank">Python</a> module
+        that helps developers quickly incorporate <a href="https://plotly.com/python/" target="_blank">Plotly</a> Gauge visualizations
+        into their projects with the help of preset parameters. The application includes multiple
+        demonstrations and interactive examples to help users become familiar with the module.  Additional information about the application, including
+        source code, dependencies, tools used for development, and parameters, can be found in the <a href="https://rev-gauge.streamlit.app/Documentation" target="_blank">documentation</a>
+        section the site.
+        </p>
 
-    """, unsafe_allow_html=True
-  )
+        """, unsafe_allow_html=True
+    )
 
-  st.markdown(
-      """
-        ![Alt Text](https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExdGNuamZlbGg2ZHVqdnMweGl0cHdmOWU1Y3Y1cXIwdnF0anptdTh6NyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/aWYNvKMvwPVADwIM4i/giphy.gif)
-      """
-  )
+    st.markdown(
+        """
+            ![Alt Text](https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExdGNuamZlbGg2ZHVqdnMweGl0cHdmOWU1Y3Y1cXIwdnF0anptdTh6NyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/aWYNvKMvwPVADwIM4i/giphy.gif)
+        """
+    )
 
-  st.divider()
+    st.divider()
 
-  st.markdown(
-      """
-      <div>
-        <a href="https://www.techbyderek.com" target="_blank">
-          <img src="https://gist.githubusercontent.com/REPNOT/183759c1eec2736531dd923d8256a782/raw/e923c91b6d9a5dde3b05c73096cf3e1d7f33b531/D%2520LOGO%2520BLACK%2520-%2520240%2520-%2520NO%2520BG.png" width="80">
-        </a>
-        &nbsp;&nbsp;&nbsp;&nbsp;
-        <a href="https://github.com/REPNOT" target="_blank">
-          <img src="https://gist.githubusercontent.com/REPNOT/183759c1eec2736531dd923d8256a782/raw/2e2ac936dc7ba38079485323bafed43346988a1a/github-mark.svg" width="50" height="50">
-        </a>
-      </div>
-      """, unsafe_allow_html=True
-  )
+    st.markdown(
+        """
+        <div>
+            <a href="https://www.techbyderek.com" target="_blank">
+            <img src="https://gist.githubusercontent.com/REPNOT/183759c1eec2736531dd923d8256a782/raw/e923c91b6d9a5dde3b05c73096cf3e1d7f33b531/D%2520LOGO%2520BLACK%2520-%2520240%2520-%2520NO%2520BG.png" width="80">
+            </a>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <a href="https://github.com/REPNOT" target="_blank">
+            <img src="https://gist.githubusercontent.com/REPNOT/183759c1eec2736531dd923d8256a782/raw/2e2ac936dc7ba38079485323bafed43346988a1a/github-mark.svg" width="50" height="50">
+            </a>
+        </div>
+        """, unsafe_allow_html=True
+    )
 
 if __name__ == "__main__":
     run()
