bcc79e5ac23c128009e92c5f242a652010929af9
Derek Evans <derek.evans@deinfoservice.com>
2023-11-02 04:58:09 -0400

    Update

--- pages/2_Interactive_Demo.py
+++ pages/2_Interactive_Demo.py
@@ -86,24 +86,19 @@ with st.expander("Gauge Settings"):
 
 
     with st.container():
-        
+
         col3, col4 = st.columns(2)
         col6, col7 = st.columns(2)
         col9, col10 = st.columns(2)
 
         with col3:
             lColor = st.color_picker('Pick a Color - Low Range', '#FF1708')
-        with col4:
             low_color = st.text_input('Color Selection - Low Range', lColor, disabled=True)
-
-        with col6:
+        with col4:
             mColor = st.color_picker('Pick a Color - Mid Range', '#FF9400')
-        with col7:
             mid_color = st.text_input('Color Selection - Mid Range', mColor, disabled=True)
-
-        with col9:
+        with col6:
             hColor = st.color_picker('Pick a Color - High Range', '#1B8720')
-        with col10:
             high_color = st.text_input("Color Selection - High Range", hColor, disabled=True)
 
 
