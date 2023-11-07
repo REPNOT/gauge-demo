ad140f3c2a86e559c13c3e175c61c3d6bff7106a
Derek Evans <derek.evans@deinfoservice.com>
2023-11-07 12:52:38 -0500

    Update

--- pages/4_Documentation.py
+++ pages/4_Documentation.py
@@ -838,34 +838,12 @@ with st.expander("Dependencies"):
             | --------- | -------- | ------------------------------------------------------------------ |
             | Streamlit | Python   | https://github.com/streamlit/streamlit                             |
             | Plotly    | Python   | https://github.com/plotly/plotly.py                                |
-            | Image     | Python   | https://pypi.org/project/image/                                    |
-            | time      | Python   | https://github.com/python/cpython/blob/main/Doc/library/time.rst   |
             | pandas    | Python   | https://github.com/pandas-dev/pandas                               |
-            | json      | Python   | https://github.com/python/cpython/blob/main/Doc/library/json.rst   |
-            | requests  | Python   | https://pypi.org/project/requests/                                 |
-            | pathlib   | Python   | https://pypi.org/project/pathlib/                                  |
-            | pprint    | Python   | https://github.com/python/cpython/blob/main/Doc/library/pprint.rst |
         """
     )
 
 
     st.write(" ")
-
-
-"## Programming Languages, Frameworks, Libraries"
-
-with st.expander("Programming Languages, Frameworks, Libraries"):
-
-    """
-        | Languages & Libraries | Link                                          |
-        | --------------------- | --------------------------------------------- |
-        | Python                | https://www.python.org/                       |
-        | Streamlit             | https://streamlit.io/                         |
-        | Plotly                | https://plotly.com/python/                    |
-        | Markdown              | https://daringfireball.net/projects/markdown/ |
-    """
-    st.write(" ")
-
 
 "## Software & Tools Used"
 
@@ -878,7 +856,6 @@ with st.expander("Software & Tools Used"):
         | Github Repositories  | https://github.com/                                           |
         | Microsoft VS Code    | https://code.visualstudio.com/                                |
         | Techsmith Camtasia   | https://www.techsmith.com/video-editor.html                   |
-        | GIPHY                | https://giphy.com/                                            |
         | git                  | https://git-scm.com/                                          |
         | Streamlit Workspaces | https://share.streamlit.io/                                   |
         | Typedown 1.2.18.0    | https://apps.microsoft.com/detail/9P8TCW4H2HB4?hl=en-us&gl=US |
